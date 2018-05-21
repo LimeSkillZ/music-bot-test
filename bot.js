bd9429e473b38221af6e4abf725a5b18882e5efa
LimeSkillZ <38006438+LimeSkillZ@users.noreply.github.com>
2018-05-20 20:26:27 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,11 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') {
     	message.reply('pong');
+
+client.on('message', message => {
+    if (message.content === 'Hippity Hoppity') {
+    	message.reply('Women are property');
+  	}
   	}
 });
 
